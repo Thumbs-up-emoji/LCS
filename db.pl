afcca1ccffcf99e865745fc749ecb47c205fe2c1
Vineet Krishna <f20221197@goa.bits-pilani.ac.in>
2023-10-19 01:24:10 +0530

    Update db.pl

--- db.pl
+++ db.pl
@@ -67,3 +67,12 @@ even([_| Rest]):-
     odd(Rest).
 odd([_| Rest]) :- 
     even(Rest).
+
+numeral(0).
+numeral(succ(X)) :- 
+    numeral(X).
+greater_than(succ(0), 0).
+greater_than(succ(X), Y):-
+    greater_than(X, Y).
+greater_than(succ(X), succ(Y)) :-
+    greater_than(X, Y).
