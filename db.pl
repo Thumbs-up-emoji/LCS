5fd0fe314181b55d0527feff013fd21ea5f29583
Vineet Krishna <f20221197@goa.bits-pilani.ac.in>
2023-10-19 01:27:37 +0530

    Update db.pl

--- db.pl
+++ db.pl
@@ -76,3 +76,22 @@ greater_than(succ(X), Y):-
     greater_than(X, Y).
 greater_than(succ(X), succ(Y)) :-
     greater_than(X, Y).
+
+list_sum([],0).
+list_sum([Head|Tail], Sum) :-
+    list_sum(Tail,SumTemp),
+    Sum is Head + SumTemp.
+
+max_of_two(X,Y,X) :- 
+    X >= Y.
+max_of_two(X,Y,Y) :- 
+    X < Y.
+list_max_elem([X],X).
+list_max_elem([X,Y|Rest],Max) :-
+    list_max_elem([Y|Rest],MaxRest),
+    max_of_two(X,MaxRest,Max).
+
+list_divide([],[],[]).
+list_divide([X],[X],[]).
+list_divide([X,Y|Tail], [X|List1],[Y|List2]) :-
+    list_divide(Tail,List1,List2).
