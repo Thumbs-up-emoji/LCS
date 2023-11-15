3ec22bdb84c27213591d123d9ef2c1238bf4d808
Vineet Krishna <f20221197@goa.bits-pilani.ac.in>
2023-11-15 23:40:02 +0530

    Update t1.pl

--- t1.pl
+++ t1.pl
@@ -27,3 +27,18 @@ myfirst(X,[X|_]).
 % (f) mytwice(X,Y) returns true iff the left argument X is a list, and the right argument Y is a list consisting of every element in X written twice.
 mytwice([],[]).
 mytwice([H|T],[H,H|Y]) :- mytwice(T,Y).
+
+% Define the merge predicate
+merge([], L, L).
+merge(L, [], L).
+merge([H1|T1], [H2|T2], [H1|M]) :- H1 =< H2, merge(T1, [H2|T2], M).
+merge([H1|T1], [H2|T2], [H2|M]) :- H1 > H2, merge([H1|T1], T2, M).
+
+% Define the predicate
+list_to_num(L, N) :- list_to_num(L, 0, N).
+
+% Base case: An empty list results in the accumulator
+list_to_num([], Acc, Acc).
+
+% Recursive case: Multiply the accumulator by 10, add the current digit, and recurse on the tail of the list
+list_to_num([H|T], Acc, N) :- NewAcc is Acc * 10 + H, list_to_num(T, NewAcc, N).
